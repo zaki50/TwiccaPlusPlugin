674ddf2aeaf384883b3d96655103cec36ee185f9
YAMAZAKI Makoto <makoto1975@gmail.com>
2011-12-02 19:24:20 +0900

    最近の G+アプリのアクティビティ名変更に対応しました

--- src/org/zakky/twicca/plus/TwiccaPlusPluginActivity.java
+++ src/org/zakky/twicca/plus/TwiccaPlusPluginActivity.java
@@ -17,14 +17,23 @@
 package org.zakky.twicca.plus;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Debug;
 
 public class TwiccaPlusPluginActivity extends Activity {
     private static final int REQ_PLUS = 1;
 
     private static final int REQ_INSTALL = 2;
+
+    private static final String PACKAGE_NAME = "com.google.android.apps.plus";
+
+    private static final String OLD_ACTIVITY_CLASS_NAME = "com.google.android.apps.plusone.app.ComposeUpdateActivity";
+
+    // Post するための Activity の名前。今のところタブレットでも phone らしい
+    private static final String ACTIVITY_CLASS_NAME_FOR_PHONE = PACKAGE_NAME + ".phone.PostActivity";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,7 @@ public class TwiccaPlusPluginActivity extends Activity {
 
     @Override
     protected void onStart() {
+        Debug.waitForDebugger();
         super.onStart();
 
         final Intent initiatorIntent = getIntent();
@@ -49,17 +59,44 @@ public class TwiccaPlusPluginActivity extends Activity {
             return;
         }
 
+        final Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+
+        // まずは、最新の G+ アプリの Activity を試す。
+        intent.setClassName(PACKAGE_NAME, ACTIVITY_CLASS_NAME_FOR_PHONE);
+        if (startActivityForResult(this, intent, REQ_PLUS)) {
+            return;
+        }
+
+        // ダメだった場合は 以前のバージョンの Activity を試す。
+        intent.setClassName(PACKAGE_NAME, OLD_ACTIVITY_CLASS_NAME);
+        if (startActivityForResult(this, intent, REQ_PLUS)) {
+            return;
+        }
+
+        // 見つからなかったのでマーケットへ飛ばす
+        Uri uri = Uri.parse("market://search?q=" + PACKAGE_NAME);
+        Intent marketIntent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivityForResult(marketIntent, REQ_INSTALL);
+    }
+
+    /**
+     * {@link Activity#startActivityForResult(Intent, int)} を実行し、
+     * インテントをうけとる Activity が存在したかどうかを返り値として返します。
+     *
+     * @param activity startActivityForResult を実行するアクティビティ。
+     * @param intent 投げるインテント。
+     * @param requestCode resultを受け取る際のリクエストコード。
+     * @return 受け取るアクティビティが∃した場合は {@code true} を、存在せずに
+     * {@link ActivityNotFoundException} がスローされた場合は {@code false}を返します。
+     */
+    private static boolean startActivityForResult(Activity activity, Intent intent, int requestCode) {
         try {
-            final Intent intent = new Intent(Intent.ACTION_SEND);
-            intent.setType("text/plain");
-            intent.putExtra(Intent.EXTRA_TEXT, text);
-            intent.setClassName("com.google.android.apps.plus",
-                    "com.google.android.apps.plusone.app.ComposeUpdateActivity");
-            startActivityForResult(intent, REQ_PLUS);
-        } catch (Exception e) {
-            Uri uri = Uri.parse("market://search?q=com.google.android.apps.plus");
-            Intent marketIntent = new Intent(Intent.ACTION_VIEW, uri);
-            startActivityForResult(marketIntent, REQ_INSTALL);
+            activity.startActivityForResult(intent, requestCode);
+            return true;
+        } catch (ActivityNotFoundException e) {
+            return false;
         }
     }
 
